d703025a3274234dc3c06f4f82d175ff1580c24f
Ian Johnson <ianjohnson754@gmail.com>
2025-03-25 18:00:39 +0000

    chore: Updating description to be TEXT instead of VARCHAR

--- src/main/java/uk/co/powdr/model/TodoItem.java
+++ src/main/java/uk/co/powdr/model/TodoItem.java
@@ -20,6 +20,7 @@ public class TodoItem {
     @Column(nullable = false)
     private String title;
 
+    @Column(columnDefinition = "TEXT")
     private String description;
 
     @Column(nullable = false)
